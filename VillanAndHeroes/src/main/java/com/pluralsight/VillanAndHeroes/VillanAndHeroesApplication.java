5ac4b33cbdb549d226a3d0ee5f1da595dd884c00
Bilenie Tilahun <bilumekb@gmail.com>
2025-06-16 20:25:18 -0500

    VillanAndHeroes.

---
+++ VillanAndHeroes/src/main/java/com/pluralsight/VillanAndHeroes/VillanAndHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.VillanAndHeroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillanAndHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillanAndHeroesApplication.class, args);
+	}
+
+}
---
+++ VillanAndHeroes/src/test/java/com/pluralsight/VillanAndHeroes/VillanAndHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.VillanAndHeroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillanAndHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
